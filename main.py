08ac8d0d471e692c75385d98eedb94994c839b1f
MartinP96 <porenta.martin@gmail.com>
2023-05-12 00:35:32 +0300

    Teting flask

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask
-
+from iot_web_interface import sql_client
+import csv
 
 if __name__ == '__main__':
     print('test')
+
+    # Create MySQL client
+    client = sql_client.MySqlClient()
+
+    # Read SQL configuration
+    with open("configuration/sql_configuration.csv") as f:
+        reader = csv.DictReader(f)
+        sql_configuration_dict = next(reader)
+
+    # Connect to SQL
+    ret = client.connect_sql(host=sql_configuration_dict["host"],
+                             database=sql_configuration_dict["database"],
+                             user=sql_configuration_dict["user"],
+                             password=sql_configuration_dict["password"])
+
     app = Flask(__name__)
 
     @app.route('/')
@@ -17,5 +33,14 @@ if __name__ == '__main__':
         }
         return data
 
-    app.run(host="192.168.0.101")
+    @app.route('/device_list')
+    def get_device_list():
+        data = client.execute_stored_procedure("GetDeviceList")
+        return data
 
+    @app.route('/device_last_measurement')
+    def get_last_measurements_for_device():
+        data = client.execute_stored_procedure("GetLastMeasurementForDevice", (1,))
+        return data
+
+    app.run(host="192.168.0.101")
